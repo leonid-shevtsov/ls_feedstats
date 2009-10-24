1df6b20a1158fad0327963e9c19f96718c867d9a
Leonid Shevtsov <leonid@shevtsov.me>
2009-10-24 20:13:01 +0300

    initial commit

---
+++ feedstats.php
@@ -0,0 +1,78 @@
+<?php
+/*
+Plugin Name: LSFeedStats
+Plugin URI: http://leonid.shevtsov.me
+Description: Feed statistics by Leonid Shevtsov
+Version: 0.1
+Author: Leonid Shevtsov
+Author URI: http://leonid.shevtsov.me
+
+    Copyright 2009  Leonid Shevtsov  (email : leonid@shevtsov.me)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+$ls_feedstats_visits_table_name = "{$wpdb->prefix}feedvisits";
+
+function ls_feedstats_install()
+{
+  global $wpdb;
+  global $ls_feedstats_visits_table_name;
+
+  $sql = "CREATE TABLE {$ls_feedstats_visits_table_name} (
+    id int NOT NULL AUTO_INCREMENT,
+    time int NOT NULL,
+    ip varchar(15) NOT NULL,
+    referer varchar(255) NOT NULL,
+    useragent varchar(255) NOT NULL,
+    UNIQUE KEY id (id)
+  );";
+
+  require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+  dbDelta($sql);
+}
+
+
+// Client IP, considering proxy masquerading
+function ls_feedstats_get_client_ip()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+
+    return $ip;
+}
+
+
+//The method that collects statistics
+function ls_feedstats_visit()
+{
+  global $wpdb;
+  global $ls_feedstats_visits_table_name;
+
+  $wpdb->insert($ls_feedstats_visits_table_name, array(
+    'time' => time(),
+    'ip' => ls_feedstats_get_client_ip(),
+    'referer'=> isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '',
+    'useragent'=> isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : ''
+   ), array('%d','%s','%s','%s'));
+}
+
+# Hooks
+register_activation_hook(__FILE__,'ls_feedstats_install');
+add_action('do_feed_rss2', 'ls_feedstats_visit', 1, false);
---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== Feed Stats ===
+Contributors: lshevtsov
+Donate link: http://leonid.shevtsov.me
+Tags: rss
+Requires at least: 2.7.5
+Tested up to: 2.7.5
+Stable tag: trunk
+
+Best feed stats
+
+== Description ==
+
+TODO
+
+== Installation ==
+
+TODO
+
+== Frequently Asked Questions ==
+
+= A question that someone might have =
+
+An answer to that question.
+
+= What about foo bar? =
+
+Answer to foo bar dilemma.
+
+== Screenshots ==
+
+1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
+the directory of the stable readme.txt, so in this case, `/tags/4.3/screenshot-1.png` (or jpg, jpeg, gif)
+2. This is the second screen shot
+
+== Changelog ==
+
+= 1.0 =
+* A change since the previous version.
+* Another change.
+
+= 0.5 =
+* List versions from most recent at top to oldest at bottom.
